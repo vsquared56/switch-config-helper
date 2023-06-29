2e1f937595c33ef382c57f4fd79491ceb6968826
Vlad Voskoboynikov <vladv@vladv.org>
2023-06-28 22:43:46 -0500

    Add basic cottle example

--- TestSampleCmdletCommand.cs
+++ TestSampleCmdletCommand.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
+using Cottle;
+using System.Collections.Generic;
 
 namespace Switch_Config_Helper
 {
@@ -30,6 +32,17 @@ namespace Switch_Config_Helper
         // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
         protected override void ProcessRecord()
         {
+            var template = "Hello {who}, stay awhile and listen!";
+
+            var documentResult = Document.CreateDefault(template); // Create from template string
+            var document = documentResult.DocumentOrThrow; // Throws ParseException on error
+
+            var context = Context.CreateBuiltin(new Dictionary<Value, Value>
+            {
+                ["who"] = "my friend" // Declare new variable "who" with value "my friend"
+            });
+
+            FavoritePet = document.Render(context);
             WriteObject(new FavoriteStuff { 
                 FavoriteNumber = FavoriteNumber,
                 FavoritePet = FavoritePet
