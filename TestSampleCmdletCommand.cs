f54022e52bc245b5db07091385fc75ce261f66fc
Vlad Voskoboynikov <vladv@vladv.org>
2023-07-02 23:26:20 -0500

    Read cottle template from file

--- TestSampleCmdletCommand.cs
+++ TestSampleCmdletCommand.cs
@@ -3,11 +3,12 @@ using System.Management.Automation;
 using System.Management.Automation.Runspaces;
 using Cottle;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Switch_Config_Helper
 {
-    [Cmdlet(VerbsDiagnostic.Test,"SampleCmdlet")]
-    [OutputType(typeof(FavoriteStuff))]
+    [Cmdlet(VerbsDiagnostic.Test, "SampleCmdlet")]
+    [OutputType(typeof(string))]
     public class TestSampleCmdletCommand : PSCmdlet
     {
         [Parameter(
@@ -15,50 +16,34 @@ namespace Switch_Config_Helper
             Position = 0,
             ValueFromPipeline = true,
             ValueFromPipelineByPropertyName = true)]
-        public int FavoriteNumber { get; set; }
+        public string TemplatePath { get; set; }
 
-        [Parameter(
-            Position = 1,
-            ValueFromPipelineByPropertyName = true)]
-        [ValidateSet("Cat", "Dog", "Horse")]
-        public string FavoritePet { get; set; } = "Dog";
-
-        // This method gets called once for each cmdlet in the pipeline when the pipeline starts executing
         protected override void BeginProcessing()
         {
-            WriteVerbose("Begin!");
+
         }
 
-        // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
         protected override void ProcessRecord()
         {
-            var template = "Hello {who}, stay awhile and listen!";
-
-            var documentResult = Document.CreateDefault(template); // Create from template string
-            var document = documentResult.DocumentOrThrow; // Throws ParseException on error
-
-            var context = Context.CreateBuiltin(new Dictionary<Value, Value>
+            using (StreamReader templateReader = File.OpenText(TemplatePath))
             {
-                ["who"] = "my friend" // Declare new variable "who" with value "my friend"
-            });
+                var documentResult = Document.CreateDefault(templateReader); // Create from template string
+                var document = documentResult.DocumentOrThrow; // Throws ParseException on error
 
-            FavoritePet = document.Render(context);
-            WriteObject(new FavoriteStuff { 
-                FavoriteNumber = FavoriteNumber,
-                FavoritePet = FavoritePet
-            });
+                var context = Context.CreateBuiltin(new Dictionary<Value, Value>
+                {
+                    ["who"] = "my friend" // Declare new variable "who" with value "my friend"
+                });
+
+                var output = document.Render(context);
+                WriteObject(output);
+            }
         }
 
         // This method will be called once at the end of pipeline execution; if no input is received, this method is not called
         protected override void EndProcessing()
         {
-            WriteVerbose("End!");
-        }
-    }
 
-    public class FavoriteStuff
-    {
-        public int FavoriteNumber { get; set; }
-        public string FavoritePet { get; set; }
+        }
     }
 }
