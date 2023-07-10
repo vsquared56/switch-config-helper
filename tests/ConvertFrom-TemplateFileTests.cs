303aa1716a9607e85d6b25694b3e1a8fa5ddb7a0
Vlad Voskoboynikov <vladv@vladv.org>
2023-07-09 21:51:37 -0500

    Add test project

---
+++ tests/ConvertFrom-TemplateFileTests.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace SwitchConfigHelper.Tests
+{
+	public class ConvertFromTemplateFileTests
+	{
+		public class ConvertFromTemplateFile
+		{
+			public class DnsLookupsTests
+			{
+				[Fact]
+				public void ResolveATestLocalhost()
+				{
+					// Arrange.
+					var path = "simple_dns_test.txt";
+					var cmdlet = new ConvertFromTemplateFileCommand()
+					{
+						TemplatePath = path,
+					};
+					var expectedResult = "127.0.0.1";
+
+					// Act.
+					var results = cmdlet.Invoke().OfType<string>().ToList();
+
+					// Assert.
+					Assert.Equal(results.First(), expectedResult);
+					Assert.True(results.Count == 1);
+				}
+			}
+		}
+	}
+}
