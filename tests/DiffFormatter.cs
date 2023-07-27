815f48dd218ccf45f19e26a067a833d79fe99ebe
Vlad Voskoboynikov <vladv@vladv.org>
2023-07-26 23:30:45 -0500

    Add failing section headers test

--- tests/DiffFormatter.cs
+++ tests/DiffFormatter.cs
@@ -35,6 +35,56 @@ namespace SwitchConfigHelper.Tests
                 var diffOutput = DiffFormatter.FormatDiff(model, false);
                 Assert.Equal(expectedOutput, diffOutput);
             }
+
+            [Fact]
+            public void SimpleInsertAndDelete()
+            {
+                var model = new DiffPaneModel();
+                model.Lines.Add(new DiffPiece("section 1", ChangeType.Unchanged, 1));
+                model.Lines.Add(new DiffPiece("statement 1.1", ChangeType.Unchanged, 2));
+                model.Lines.Add(new DiffPiece("statement 1.2", ChangeType.Inserted, 3));
+                model.Lines.Add(new DiffPiece("!", ChangeType.Unchanged, 4));
+                model.Lines.Add(new DiffPiece("section 2", ChangeType.Unchanged, 5));
+                model.Lines.Add(new DiffPiece("statement 2.1", ChangeType.Unchanged, 6));
+                model.Lines.Add(new DiffPiece("statement 2.2", ChangeType.Deleted, 7));
+                model.Lines.Add(new DiffPiece("!", ChangeType.Unchanged, 8));
+
+                string expectedOutput = @"  section 1
+  statement 1.1
++ statement 1.2
+  !
+  section 2
+  statement 2.1
+- statement 2.2
+  !
+";
+
+                var diffOutput = DiffFormatter.FormatDiff(model, false);
+                Assert.Equal(expectedOutput, diffOutput);
+            }
+
+            [Fact]
+            public void SectionHeaders()
+            {
+                var model = new DiffPaneModel();
+                model.Lines.Add(new DiffPiece("section 1", ChangeType.Unchanged, 1));
+                model.Lines.Add(new DiffPiece("statement 1.1", ChangeType.Unchanged, 2));
+                model.Lines.Add(new DiffPiece("statement 1.2", ChangeType.Inserted, 3));
+                model.Lines.Add(new DiffPiece("!", ChangeType.Unchanged, 4));
+                model.Lines.Add(new DiffPiece("section 2", ChangeType.Unchanged, 5));
+                model.Lines.Add(new DiffPiece("statement 2.1", ChangeType.Unchanged, 6));
+                model.Lines.Add(new DiffPiece("statement 2.2", ChangeType.Deleted, 7));
+                model.Lines.Add(new DiffPiece("!", ChangeType.Unchanged, 8));
+
+                string expectedOutput = @"  section 1
++ statement 1.2
+  section 2
+- statement 2.2
+";
+
+                var diffOutput = DiffFormatter.FormatDiff(model, false, 0, true);
+                Assert.Equal(expectedOutput, diffOutput);
+            }
         }
     }
 }
