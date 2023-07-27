79fad1d2b4500a20137a343648638d6d29e35ca0
Vlad Voskoboynikov <vladv@vladv.org>
2023-07-26 23:25:25 -0500

    Simple formatter test with no changes

---
+++ tests/DiffFormatter.cs
@@ -0,0 +1,40 @@
+using Xunit;
+using DiffPlex;
+using DiffPlex.DiffBuilder.Model;
+using SwitchConfigHelper;
+
+namespace SwitchConfigHelper.Tests
+{
+    public class DiffFormatterTests
+    {
+        public class FormatWithoutLineNumbers
+        {
+            [Fact]
+            public void NoChanges()
+            {
+                var model = new DiffPaneModel();
+                model.Lines.Add(new DiffPiece("section 1", ChangeType.Unchanged, 1));
+                model.Lines.Add(new DiffPiece("statement 1.1", ChangeType.Unchanged, 2));
+                model.Lines.Add(new DiffPiece("statement 1.2", ChangeType.Unchanged, 3));
+                model.Lines.Add(new DiffPiece("!", ChangeType.Unchanged, 4));
+                model.Lines.Add(new DiffPiece("section 2", ChangeType.Unchanged, 5));
+                model.Lines.Add(new DiffPiece("statement 2.1", ChangeType.Unchanged, 6));
+                model.Lines.Add(new DiffPiece("statement 2.2", ChangeType.Unchanged, 7));
+                model.Lines.Add(new DiffPiece("!", ChangeType.Unchanged, 8));
+
+                string expectedOutput = @"  section 1
+  statement 1.1
+  statement 1.2
+  !
+  section 2
+  statement 2.1
+  statement 2.2
+  !
+";
+
+                var diffOutput = DiffFormatter.FormatDiff(model, false);
+                Assert.Equal(expectedOutput, diffOutput);
+            }
+        }
+    }
+}
