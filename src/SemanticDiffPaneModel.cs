53cf9a52cf61f2f6ce5a442778abb2c80c63e856
Vlad Voskoboynikov <vladv@vladv.org>
2023-08-03 18:54:51 -0500

    Models can make themselves be semantic

--- src/SemanticDiffPaneModel.cs
+++ src/SemanticDiffPaneModel.cs
@@ -20,7 +20,38 @@ namespace SwitchConfigHelper
 
         public SemanticDiffPaneModel(DiffPaneModel model)
         {
-            Lines = model.Lines.ConvertAll(x => new SemanticDiffPiece(x));
+            Lines = new List<SemanticDiffPiece>();
+            int currentSectionStart = -1;
+            for (int i = 0; i < model.Lines.Count; i++)
+            {
+                var currentLine = model.Lines[i];
+                if (currentLine.Text == "!" && (currentLine.Type == ChangeType.Unchanged || currentLine.Type == ChangeType.Inserted))
+                {
+                    Lines.Add(new SemanticDiffPiece(
+                        currentLine.Text,
+                        currentLine.Type,
+                        currentLine.Position,
+                        currentSectionStart == -1 ? i : currentSectionStart));
+                    currentSectionStart = -1;
+                }
+                else if (currentSectionStart == -1 && currentLine.Position != null && (currentLine.Type == ChangeType.Unchanged || currentLine.Type == ChangeType.Inserted))
+                {
+                    currentSectionStart = (int)currentLine.Position;
+                    Lines.Add(new SemanticDiffPiece(
+                        currentLine.Text,
+                        currentLine.Type,
+                        currentLine.Position,
+                        currentSectionStart));
+                }
+                else
+                {
+                    Lines.Add(new SemanticDiffPiece(
+                        currentLine.Text,
+                        currentLine.Type,
+                        currentLine.Position,
+                        currentSectionStart));
+                }
+            }
         }
     }
 }
--- src/SemanticInlineDiffBuilder.cs
+++ src/SemanticInlineDiffBuilder.cs
@@ -53,84 +53,51 @@ namespace SwitchConfigHelper
         //for better semantic alignment.  See 3.2.2 in https://neil.fraser.name/writing/diff/ for inspiration
         public SemanticDiffPaneModel PerformSemanticShifts(SemanticDiffPaneModel model)
         {
-            SemanticDiffPaneModel semanticModel = new SemanticDiffPaneModel();
             if (!model.HasDifferences)
             {
                 return model;
             }
             else
             {
-                int currentSectionStart = -1;
-                for (int i = 0; i < model.Lines.Count; i++)
-                {
-                    var currentLine = model.Lines[i];
-                    if (currentLine.Text == "!" && (currentLine.Type == ChangeType.Unchanged || currentLine.Type == ChangeType.Inserted))
-                    {
-                        semanticModel.Lines.Add(new SemanticDiffPiece(
-                            currentLine.Text,
-                            currentLine.Type,
-                            currentLine.Position,
-                            currentSectionStart == -1 ? i : currentSectionStart));
-                        currentSectionStart = -1;
-                    }
-                    else if (currentSectionStart == -1 && currentLine.Position != null && (currentLine.Type == ChangeType.Unchanged || currentLine.Type == ChangeType.Inserted))
-                    {
-                        currentSectionStart = (int)currentLine.Position;
-                        semanticModel.Lines.Add(new SemanticDiffPiece(
-                            currentLine.Text,
-                            currentLine.Type,
-                            currentLine.Position,
-                            currentSectionStart));
-                    }
-                    else
-                    {
-                        semanticModel.Lines.Add(new SemanticDiffPiece(
-                            currentLine.Text,
-                            currentLine.Type,
-                            currentLine.Position,
-                            currentSectionStart));
-                    }
-                }
-
                 //Detect changed blocks surrounded by unchanged ones
                 //The first and last lines do not need to be checked
                 for (var changeStart = 1; changeStart < model.Lines.Count - 1; changeStart++)
                 {
-                    if (semanticModel.Lines[changeStart - 1].Type == ChangeType.Unchanged &&
-                        (semanticModel.Lines[changeStart].Type == ChangeType.Deleted || semanticModel.Lines[changeStart].Type == ChangeType.Inserted))
+                    if (model.Lines[changeStart - 1].Type == ChangeType.Unchanged &&
+                        (model.Lines[changeStart].Type == ChangeType.Deleted || model.Lines[changeStart].Type == ChangeType.Inserted))
                     {
                         var changeEnd = changeStart;
-                        while ((changeEnd < semanticModel.Lines.Count - 1) && (semanticModel.Lines[changeEnd + 1].Type == semanticModel.Lines[changeStart].Type))
+                        while ((changeEnd < model.Lines.Count - 1) && (model.Lines[changeEnd + 1].Type == model.Lines[changeStart].Type))
                         {
                             changeEnd++;
                         }
 
                         //A changed block surrounded by unchanged ones is a candidate for shifting left/right
-                        if (changeEnd < semanticModel.Lines.Count - 1 && semanticModel.Lines[changeEnd + 1].Type == ChangeType.Unchanged)
+                        if (changeEnd < model.Lines.Count - 1 && model.Lines[changeEnd + 1].Type == ChangeType.Unchanged)
                         {
                             List<SemanticDiffShift> potentialShifts = new List<SemanticDiffShift>();
                             var currentShift = 0;
 
                             //Add the unshifted change, and try shifting left
-                            while (isValidShift(semanticModel, changeStart, changeEnd, currentShift))
+                            while (isValidShift(model, changeStart, changeEnd, currentShift))
                             {
                                 potentialShifts.Add(new SemanticDiffShift(currentShift,
-                                    changeStart + currentShift - 1 > 0 ? semanticModel.Lines[changeStart + currentShift - 1].Text : null,
-                                    semanticModel.Lines[changeStart + currentShift].Text,
-                                    semanticModel.Lines[changeEnd + currentShift].Text,
-                                    changeEnd + currentShift + 1 < semanticModel.Lines.Count ? semanticModel.Lines[changeEnd + currentShift + 1].Text : null));
+                                    changeStart + currentShift - 1 > 0 ? model.Lines[changeStart + currentShift - 1].Text : null,
+                                    model.Lines[changeStart + currentShift].Text,
+                                    model.Lines[changeEnd + currentShift].Text,
+                                    changeEnd + currentShift + 1 < model.Lines.Count ? model.Lines[changeEnd + currentShift + 1].Text : null));
                                 currentShift--;
                             }
 
                             //try shifting right
                             currentShift = 1;
-                            while (isValidShift(semanticModel, changeStart, changeEnd, currentShift))
+                            while (isValidShift(model, changeStart, changeEnd, currentShift))
                             {
                                 potentialShifts.Add(new SemanticDiffShift(currentShift,
-                                    changeStart + currentShift - 1 > 0 ? semanticModel.Lines[changeStart + currentShift - 1].Text : null,
-                                    semanticModel.Lines[changeStart + currentShift].Text,
-                                    semanticModel.Lines[changeEnd + currentShift].Text,
-                                    changeEnd + currentShift + 1 < semanticModel.Lines.Count ? semanticModel.Lines[changeEnd + currentShift + 1].Text : null));
+                                    changeStart + currentShift - 1 > 0 ? model.Lines[changeStart + currentShift - 1].Text : null,
+                                    model.Lines[changeStart + currentShift].Text,
+                                    model.Lines[changeEnd + currentShift].Text,
+                                    changeEnd + currentShift + 1 < model.Lines.Count ? model.Lines[changeEnd + currentShift + 1].Text : null));
                                 currentShift++;
                             }
 
@@ -140,18 +107,18 @@ namespace SwitchConfigHelper
                             //perform the shift
                             if (optimalShift != 0)
                             {
-                                var currentChange = semanticModel.Lines[changeStart].Type;
+                                var currentChange = model.Lines[changeStart].Type;
                                 var newStart = changeStart + optimalShift;
                                 var newEnd = changeEnd + optimalShift;
                                 for (var i = Math.Min(newStart, changeStart); i <= Math.Max(newEnd, changeEnd); i++)
                                 {
                                     if (i >= newStart && i <= newEnd)
                                     {
-                                        semanticModel.Lines[i].Type = currentChange;
+                                        model.Lines[i].Type = currentChange;
                                     }
                                     else
                                     {
-                                        semanticModel.Lines[i].Type = ChangeType.Unchanged;
+                                        model.Lines[i].Type = ChangeType.Unchanged;
                                     }
                                     //keep going from the end of the shifted change
                                     changeStart = Math.Max(newEnd, changeEnd);
@@ -160,7 +127,7 @@ namespace SwitchConfigHelper
                         }
                     }
                 }
-                return semanticModel;
+                return model;
             }
         }
 
