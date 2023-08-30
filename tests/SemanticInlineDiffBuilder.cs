b2bf683cc5936ab9b8d2313484b83f2950819634
Vlad Voskoboynikov <vladv@vladv.org>
2023-08-30 18:25:20 -0500

    Additional test

--- tests/SemanticInlineDiffBuilder.cs
+++ tests/SemanticInlineDiffBuilder.cs
@@ -803,6 +803,82 @@ ip access-list extended acl_vlan3
             }
 
             [Fact]
+            public void AllDuplicateAclsRemovedMarkedRemoved()
+            {
+                string referenceText = @"ip access-list extended acl_vlan1
+  remark Allow TCP DNS lookups from primary
+  permit tcp 172.20.1.0/24 host 8.8.8.8 eq dns
+  remark Allow TCP DNS lookups from primary
+  permit tcp 172.20.1.0/24 host 8.8.8.8 eq dns
+  remark Allow TCP DNS lookups from secondary
+  permit tcp 172.20.1.0/24 host 8.8.4.4 eq dns
+!";
+
+                string differenceText = @"ip access-list extended acl_vlan1
+  remark Allow TCP DNS lookups from secondary
+  permit tcp 172.20.1.0/24 host 8.8.4.4 eq dns
+!";
+
+                var diffBuilder = new SemanticInlineDiffBuilder(new Differ());
+                var diff = diffBuilder.BuildEffectiveDiffModel(referenceText, differenceText, true);
+
+                diff.Should().BeOfType<SemanticDiffPaneModel>();
+                diff.Lines.Should().AllBeOfType<SemanticDiffPiece>();
+                diff.Lines.Should().HaveCount(6);
+                diff.Lines.Where(x => x.Type != ChangeType.Deleted).Should().OnlyHaveUniqueItems(x => x.Position);
+                diff.Lines.Where(x => x.Type != ChangeType.Deleted && x.Type != ChangeType.Modified).Should().BeInAscendingOrder(x => x.Position);
+                diff.Lines.Should().BeInAscendingOrder(x => x.SectionStartPosition);
+
+                diff.Lines.Where(x => x.Text.Trim().StartsWith("remark"))
+                    .Should().AllSatisfy(x => x.Type.Should().BeOneOf(ChangeType.Unchanged, ChangeType.Modified));
+
+                diff.Lines.Should().SatisfyRespectively(
+                    line =>
+                    {
+                        line.Position.Should().Be(1);
+                        line.Text.Should().Be("ip access-list extended acl_vlan1");
+                        line.Type.Should().Be(ChangeType.Unchanged);
+                        line.SectionStartPosition.Should().Be(1);
+                    },
+                    line =>
+                    {
+                        line.Position.Should().BeNull();
+                        line.Text.Should().Be("  permit tcp 172.20.1.0/24 host 8.8.8.8 eq dns");
+                        line.Type.Should().Be(ChangeType.Deleted);
+                        line.SectionStartPosition.Should().Be(1);
+                    },
+                    line =>
+                    {
+                        line.Position.Should().BeNull();
+                        line.Text.Should().Be("  permit tcp 172.20.1.0/24 host 8.8.8.8 eq dns");
+                        line.Type.Should().Be(ChangeType.Deleted);
+                        line.SectionStartPosition.Should().Be(1);
+                    },
+                    line =>
+                    {
+                        line.Position.Should().Be(2);
+                        line.Text.Should().Be("  remark Allow TCP DNS lookups from secondary");
+                        line.Type.Should().Be(ChangeType.Unchanged);
+                        line.SectionStartPosition.Should().Be(1);
+                    },
+                    line =>
+                    {
+                        line.Position.Should().Be(3);
+                        line.Text.Should().Be("  permit tcp 172.20.1.0/24 host 8.8.4.4 eq dns");
+                        line.Type.Should().Be(ChangeType.Unchanged);
+                        line.SectionStartPosition.Should().Be(1);
+                    },
+                    line =>
+                    {
+                        line.Position.Should().Be(4);
+                        line.Text.Should().Be("!");
+                        line.Type.Should().Be(ChangeType.Unchanged);
+                        line.SectionStartPosition.Should().Be(1);
+                    }
+                );
+            }
+
+            [Fact]
             public void DuplicateAclsRemovedNotIgnored()
             {
                 string referenceText = @"ip access-list extended acl_vlan1
